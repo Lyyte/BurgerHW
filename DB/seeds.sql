836e974690eed88d0e8d310e5489b205e11cfc7b
Lyyte <lyytefaridi@gmail.com>
2020-07-07 20:37:59 -0500

    final commit

---
+++ DB/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgerT (name) VALUES ('Impossible Burger');
+INSERT INTO burgetT (name, devoured) VALUES ('Chicken Burger', True);
+INSERT INTO burgerT (name, devoured) VALUES ('Black Bean', True);
